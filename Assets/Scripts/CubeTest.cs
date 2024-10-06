81377adc96515e07674866531ee08ae843bb5a3f
Elliott Jang <elliottjang1@gmail.com>
2024-10-06 11:56:32 -0700

    add cubetest

---
+++ Assets/Scripts/CubeTest.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class CubeTest : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Test() {
+        Debug.Log("HI");
+    }
+}
