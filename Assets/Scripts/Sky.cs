357a3b6d1dce6dee72ba75a01c75e4e190d6a9c4
Ethan Henry <79533577+ethansocal@users.noreply.github.com>
2024-10-05 19:01:40 -0700

    Add black sphere sky

---
+++ Assets/Scripts/Sky.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Sky : MonoBehaviour
+{
+    public GameObject vrPerson;
+
+    public GameObject starPrefab;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            var instance = Instantiate(starPrefab, transform, true);
+            instance.transform.localPosition = Random.onUnitSphere;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = vrPerson.transform.position;
+    }
+}
